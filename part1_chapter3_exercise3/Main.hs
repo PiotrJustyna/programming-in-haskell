6ba62e55a11d3edab97a03524c00a7051375e568
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-18 13:41:06 +0000

    Chapter 3, exercise 3, minor corrections.

---
+++ part1_chapter3_exercise3/Main.hs
@@ -0,0 +1,55 @@
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter3_exercise3
+-- :load Main
+main = do
+    putStrLn "second [1, 2, 3]"
+    putStrLn . show $ second [1, 2, 3]
+
+    putStrLn "swap (1, 2)"
+    putStrLn . show $ swap (1, 2)
+
+    putStrLn "swap (1, \"2\")"
+    putStrLn . show $ swap (1, "2")
+
+    putStrLn "pair 1 \"2\""
+    putStrLn . show $ pair 1 "2"
+
+    putStrLn "double 1"
+    putStrLn . show $ double 1
+
+    putStrLn "palindrome [1, 2, 3, 4, 3, 2, 1]"
+    putStrLn . show $ palindrome [1, 2, 3, 4, 3, 2, 1]
+
+    putStrLn "twice twice (\\x -> x + 1) 1"
+    putStrLn . show $ twice (\x -> x + 1) 1
+
+----------------------------------------
+
+second :: Show a => [a] -> a
+second xs = head (tail xs)
+
+----------------------------------------
+
+swap :: (Show a, Show b) => (a, b) -> (b, a)
+swap (x,y) = (y,x)
+
+----------------------------------------
+
+pair :: (Show a, Show b) => a -> b -> (a, b)
+pair x y = (x,y)
+
+----------------------------------------
+
+double :: Num a => a -> a
+double x = x*2
+
+----------------------------------------
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+----------------------------------------
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
+----------------------------------------
