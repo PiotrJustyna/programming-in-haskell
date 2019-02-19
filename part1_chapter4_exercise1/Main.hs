066212270bd5ee9aea1704dae13e0924cc74cb0c
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-19 16:55:00 +0000

    Chapter 4, exercise 1.

---
+++ part1_chapter4_exercise1/Main.hs
@@ -0,0 +1,18 @@
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter4_exercise1
+-- :load Main
+main = do
+    putStrLn "halve [1, 2, 3, 4, 5, 6]"
+    putStrLn . show $ halve [1, 2, 3, 4, 5, 6]
+
+    putStrLn "halve [1, 2, 3]"
+    putStrLn . show $ halve [1, 2, 3]
+
+-- Using library functions, define a function halve :: [a] -> ([a],[a])
+-- that splits an even-lengthed list into two halves.
+-- For example:
+-- > halve [1, 2, 3, 4, 5, 6]
+-- ([1, 2, 3], [4, 5, 6])
+halve :: Show a => [a] -> ([a], [a])
+halve xs
+    | (length xs) `mod` 2 == 0 = (take ((length xs) `div` 2) xs, drop ((length xs) `div` 2) xs)
+    | otherwise = (xs, [])
