ec007c0ce7060c6298fc0f6f4a84046816c6d0f1
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-21 12:46:22 +0000

    Part 1 Chapter 4 Exercise 5.

---
+++ part1_chapter4_exercise5/Main.hs
@@ -0,0 +1,30 @@
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter4_exercise5
+-- :load Main
+main = do
+    putStrLn "weirdAnd True True"
+    putStrLn . show $ weirdAnd True True
+
+    putStrLn "weirdAnd True False"
+    putStrLn . show $ weirdAnd True False
+
+    putStrLn "weirdAnd False True"
+    putStrLn . show $ weirdAnd False True
+
+    putStrLn "weirdAnd False False"
+    putStrLn . show $ weirdAnd False False
+
+-- Without using any other library functions or operators,
+-- show how the meaning of the following pattern matching
+-- definition for logical conjunction && can be formalised
+-- using conditional expressions:
+-- True && True = True
+-- _ && _ = False
+-- Hint: use two nested conditional expressions.
+weirdAnd :: Bool -> Bool -> Bool
+weirdAnd x y =
+    if x
+    then
+        if y
+        then True
+        else False
+    else False
