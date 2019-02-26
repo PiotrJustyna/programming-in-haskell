7d794b06ddb9b3b51d9f62ac92009721534520e8
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-26 11:15:56 +0000

    Part 1 Chapter 5 Ceasar Cipher Part 2.

--- part1_chapter5_ceasar_cipher/Main.hs
+++ part1_chapter5_ceasar_cipher/Main.hs
@@ -1,4 +1,8 @@
+-- option 1 (https://github.com/PiotrJustyna/haskell-anywhere):
 -- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter5_ceasar_cipher
+-- option 2 (stack):
+-- stack ghci
+--
 -- :load Main
 import Data.Char
 
@@ -27,6 +31,18 @@ main = do
     putStrLn "encode (-3) \"kdvnhoo lv ixq\""
     putStrLn . show $ encode (-3) "kdvnhoo lv ixq"
 
+    putStrLn "percent 5 15"
+    putStrLn . show $ percent 5 15
+
+    putStrLn "numberOfLowercaseCharacters \"Haskell\""
+    putStrLn . show $ numberOfLowercaseCharacters "Haskell"
+
+    putStrLn "count 'l' \"Haskell\""
+    putStrLn . show $ count 'l' "Hasell"
+
+    putStrLn "frequencies \"Haskell\""
+    putStrLn . show $ frequencies "Haskell"
+
 lettterToInt :: Char -> Int
 lettterToInt x = (ord x) - (ord 'a')
 
@@ -41,3 +57,16 @@ shift shiftFactor character
 
 encode :: Int -> String -> String
 encode shiftFactor text = [shift shiftFactor x | x <- text]
+
+percent :: Int -> Int -> Float
+percent x y = ((fromIntegral x) / (fromIntegral y)) * 100
+
+numberOfLowercaseCharacters :: String -> Int
+numberOfLowercaseCharacters text = length $ filter isLower text
+
+count :: Char -> String -> Int
+count characterToCount text = length $ filter (\x -> x == characterToCount) text
+
+frequencies :: String -> [(Char, Float)]
+frequencies text =
+    [(x, percent (count x text) (numberOfLowercaseCharacters text)) | x <- ['a' .. 'z']]
