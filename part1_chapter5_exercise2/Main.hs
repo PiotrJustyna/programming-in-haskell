33ea3d0934c7c098011b5f6457d537a61e5b77b1
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-27 15:14:12 +0000

    Part 1 Chapter 5 Exercises 2 and 3.

---
+++ part1_chapter5_exercise2/Main.hs
@@ -0,0 +1,11 @@
+-- option 1 (https://github.com/PiotrJustyna/haskell-anywhere):
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter5_exercise2
+-- option 2 (stack):
+-- stack ghci
+--
+-- :load Main
+main = do
+    putStrLn . show $ grid 1 2
+
+grid :: Int -> Int -> [(Int, Int)]
+grid m n = [(x, y) | y <- [0 .. n], x <- [0 .. m]]
---
+++ part1_chapter5_exercise3/Main.hs
@@ -0,0 +1,11 @@
+-- option 1 (https://github.com/PiotrJustyna/haskell-anywhere):
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter5_exercise3
+-- option 2 (stack):
+-- stack ghci
+--
+-- :load Main
+main = do
+    putStrLn . show $ square 2
+
+square :: Int -> [(Int, Int)]
+square m = [(x, y) | y <- [0 .. m], x <- [0 .. m], x /= y]
