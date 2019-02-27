a5178a95f48a0d74ac97f3ad5f77166dee3a2b70
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-27 15:31:21 +0000

    Part 1 Chapter 5 Exercise 4.

---
+++ part1_chapter5_exercise4/Main.hs
@@ -0,0 +1,12 @@
+-- option 1 (https://github.com/PiotrJustyna/haskell-anywhere):
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter5_exercise3
+-- option 2 (stack):
+-- stack ghci
+--
+-- :load Main
+main = do
+    putStrLn "replicate' 3 True"
+    putStrLn . show $ replicate' 3 True
+
+replicate' :: Show a => Int -> a -> [a]
+replicate' x y = [y | _ <- [1 .. x]]
