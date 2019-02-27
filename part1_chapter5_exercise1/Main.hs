6e048aa08ceaa4f4b1584718903f8f4472986822
Piotr Justyna <piotr.justyna@gmail.com>
2019-02-27 14:26:47 +0000

    Part 1 Chapter 5 Exercise 1.

---
+++ part1_chapter5_exercise1/Main.hs
@@ -0,0 +1,8 @@
+-- option 1 (https://github.com/PiotrJustyna/haskell-anywhere):
+-- ./ghci.bat C:\Users\piotr_justyna\Documents\github\programming-in-haskell\part1_chapter5_exercise1
+-- option 2 (stack):
+-- stack ghci
+--
+-- :load Main
+main = do
+    putStrLn . show $ sum [x ^ 2 | x <- [1 .. 100]]
