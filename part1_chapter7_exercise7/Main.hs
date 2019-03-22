806f47ed1c2bda316f47e7edfd30e51ce094a74a
Piotr Justyna <piotr.justyna@gmail.com>
2019-03-22 10:12:04 +0000

    Part 1 Chapter 7 Exercise 7, part 3.

--- part1_chapter7_exercise7/Main.hs
+++ part1_chapter7_exercise7/Main.hs
@@ -26,11 +26,32 @@ main = do
     putStrLn "encode \"abc\""
     putStrLn . show $ encode "abc"
 
+    putStrLn "encodewithParityBit \"abc\""
+    putStrLn . show $ encodewithParityBit "abc"
+
     putStrLn "chop8 [1, 0, 1, 0, 1, 0, 1, 0, 1, 1]"
     putStrLn . show $ chop8 [1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
 
     putStrLn "decode [0,1,1,0,0,0,0,1,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,1]"
     putStrLn . show $ decode [0,1,1,0,0,0,0,1,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,1]
+
+    putStrLn "decodeWithParityBit [0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,0,1,0,1,1,0,0,0,1,1,0]"
+    putStrLn . show $ decodeWithParityBit [0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,0,1,0,1,1,0,0,0,1,1,0]
+
+    putStrLn "transmit \"Haskell\""
+    putStrLn . show $ transmit "Haskell"
+
+    putStrLn "addParityBit [0, 0, 0]"
+    putStrLn . show $ addParityBit [0, 0, 0]
+
+    putStrLn "addParityBit [1, 1, 0]"
+    putStrLn . show $ addParityBit [1, 1, 0]
+
+    putStrLn "addParityBit [1, 1, 1]"
+    putStrLn . show $ addParityBit [1, 1, 1]
+
+    putStrLn "transmitWithParityBit \"Haskell\""
+    putStrLn . show $ transmitWithParityBit "Haskell"
 
 bit2Int :: [Bit] -> Int
 bit2Int bits = foldr (\x y -> x + (2 * y)) 0 (reverse bits)
@@ -42,16 +63,57 @@ int2bit 0 = []
 int2bit x = (int2bit (x `div` 2)) ++ [x `mod` 2]
 
 make8 :: [Bit] -> [Bit]
-make8 bits = take 8 (padding ++ bits)
+make8 = makeN 8
+
+make9 :: [Bit] -> [Bit]
+make9 = makeN 9
+
+makeN :: Int -> [Bit] -> [Bit]
+makeN n bits = take n (padding ++ bits)
     where
-        padding = take (8 - (length bits)) (repeat 0)
+        padding = take (n - (length bits)) (repeat 0)
+
+addParityBit :: [Bit] -> [Bit]
+addParityBit xs = xs ++ [parityBit]
+    where
+        numberOfOnes = length (filter (==1) xs)
+        parityBit = numberOfOnes `mod`2
 
 encode :: String -> [Bit]
 encode = concat . map (make8 . int2bit . ord)
 
+encodewithParityBit :: String -> [Bit]
+encodewithParityBit = concat . map (addParityBit . make8 . int2bit . ord)
+
 chop8 :: [Bit] -> [[Bit]]
-chop8 [] = []
-chop8 xs = (take 8 xs) : (chop8 (drop 8 xs))
+chop8 = chopN 8
+
+chop9 :: [Bit] -> [[Bit]]
+chop9 = chopN 9
+
+chopN :: Int -> [Bit] -> [[Bit]]
+chopN _ [] = []
+chopN n xs = (take n xs) : (chopN n (drop n xs))
 
 decode :: [Bit] -> String
 decode bits = map (chr . bit2Int . make8) (chop8 bits)
+
+decodeWithParityBit :: [Bit] -> String
+decodeWithParityBit bits = map (chr . bit2Int . checkParityBit . make9) (chop9 bits)
+
+checkParityBit :: [Bit] -> [Bit]
+checkParityBit xs = if (calculatedParityBit == givenParityBit) then dataWithoutTheParityBit else (error "Parity bit does not match.")
+        where
+            dataWithoutTheParityBit = init xs
+            numberOfOnes = length (filter (==1) dataWithoutTheParityBit)
+            calculatedParityBit = numberOfOnes `mod`2
+            givenParityBit = last xs
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+transmitWithParityBit :: String -> String
+transmitWithParityBit = decodeWithParityBit . channel . encodewithParityBit
+
+channel :: a -> a
+channel = id
