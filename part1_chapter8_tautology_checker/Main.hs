0503168e214109e047f2cb7f578c083c14083dba
Piotr Justyna <piotr.justyna@gmail.com>
2019-03-26 09:12:51 +0000

    Part 1 Chapter 8 Tautology Checker, part 3.

--- part1_chapter8_tautology_checker/Main.hs
+++ part1_chapter8_tautology_checker/Main.hs
@@ -14,18 +14,53 @@ main = do
     putStrLn . show $ And (Const True) (Const False)
     putStrLn . show $ Imply (Const True) (Const False)
 
+    putStrLn "find 'A' substitutions"
+    putStrLn . show $ find 'A' substitutions
+
     putStrLn "evaluate substitutions (Const True)"
     putStrLn . show $ evaluate substitutions (Const True)
 
-    putStrLn "evaluate ([] :: Substitution) (Const True)"
-    putStrLn . show $ evaluate ([] :: Substitution) (Const True)
+    putStrLn "evaluate ([] :: Substitutions) (Const True)"
+    putStrLn . show $ evaluate ([] :: Substitutions) (Const True)
+
+    putStrLn "evaluate substitutions (Var 'A')"
+    putStrLn . show $ evaluate substitutions (Var 'A')
+
+    putStrLn "evaluate substitutions (Var 'B')"
+    putStrLn . show $ evaluate substitutions (Var 'B')
+
+    putStrLn "evaluate substitutions (Not (Var 'A'))"
+    putStrLn . show $ evaluate substitutions (Not (Var 'A'))
+
+    putStrLn "evaluate substitutions (Not (Var 'B'))"
+    putStrLn . show $ evaluate substitutions (Not (Var 'B'))
+
+    putStrLn "evaluate substitutions (And (Var 'A') (Var 'A'))"
+    putStrLn . show $ evaluate substitutions (And (Var 'A') (Var 'A'))
+
+    putStrLn "evaluate substitutions (And (Var 'A') (Var 'B'))"
+    putStrLn . show $ evaluate substitutions (And (Var 'A') (Var 'B'))
+
+    putStrLn "evaluate substitutions (Imply (Var 'A') (Var 'A'))"
+    putStrLn . show $ evaluate substitutions (Imply (Var 'A') (Var 'A'))
+
+    putStrLn "evaluate substitutions (Imply (Var 'B') (Var 'B'))"
+    putStrLn . show $ evaluate substitutions (Imply (Var 'B') (Var 'B'))
+
+    putStrLn "evaluate substitutions (Imply (Var 'A') (Var 'B'))"
+    putStrLn . show $ evaluate substitutions (Imply (Var 'A') (Var 'B'))
+
+    putStrLn "evaluate substitutions (Imply (Var 'B') (Var 'A'))"
+    putStrLn . show $ evaluate substitutions (Imply (Var 'B') (Var 'A'))
 
     where
         substitutions = [('A', True), ('B', False)]
 
-type Association key value = [(key, value)]
+type Association key value = (key, value)
 
-type Substitution = Association Char Bool
+type Associations key value = [Association key value]
+
+type Substitutions = Associations Char Bool
 
 data Proposition =
     Const Bool
@@ -35,5 +70,12 @@ data Proposition =
     | Imply Proposition Proposition
     deriving Show
 
-evaluate :: Substitution -> Proposition -> Bool
-evaluate _ (Const x) = x
+find :: Char -> Substitutions -> Bool
+find x substitutions = snd $ head (filter (\(key, value) -> x == key) substitutions)
+
+evaluate :: Substitutions -> Proposition -> Bool
+evaluate _ (Const constantValue) = constantValue
+evaluate substitutions (Var variableName) = find variableName substitutions
+evaluate substitutions (Not proposition) = not (evaluate substitutions proposition)
+evaluate substitutions (And proposition1 proposition2) = (evaluate substitutions proposition1) && (evaluate substitutions proposition2)
+evaluate substitutions (Imply proposition1 proposition2) = (evaluate substitutions proposition1) <= (evaluate substitutions proposition2)
