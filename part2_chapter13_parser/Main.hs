0a7a21a21a57773785da9fb02e48882619db0c8f
Piotr Justyna <piotr.justyna@gmail.com>
2020-03-13 21:17:39 +0000

    Part 2 chapter 13 - part 3, monad.

--- part2_chapter13_parser/Main.hs
+++ part2_chapter13_parser/Main.hs
@@ -26,6 +26,15 @@ instance Applicative Parser where
         [] -> []
         [(function, out)] -> parse (fmap function parser) out)
 
+instance Monad Parser where
+    -- return :: a -> Parser a
+    return = pure
+
+    -- (>>=) :: (Parser a) -> (a -> Parser b) -> (Parser b)
+    (>>=) parser functionToParser = P (\input -> case parse parser input of
+        [] -> []
+        [(value, out)] -> parse (functionToParser value) out)
+
 parse :: Parser a -> String -> [(a, String)]
 parse (P p) inp = p inp
 
@@ -33,6 +42,20 @@ item :: Parser Char
 item = P (\inp -> case inp of
     [] -> []
     (x:xs) -> [(x, xs)])
+
+three :: Parser (Char, Char)
+three = pure g <*> item <*> item <*> item
+    where
+        g x y z = (x, z)
+
+lengthParser :: Parser (String -> Int)
+lengthParser = P (\inp -> [(length, inp)])
+
+crappyParser :: Parser String
+crappyParser = P (\inp -> [(inp, inp)])
+
+uselessParser :: Char -> (Parser String)
+uselessParser x = P (\inp -> [((x:inp), inp)])
 
 main = do
     putStrLn "Parser:"
@@ -45,3 +68,10 @@ main = do
 
     putStrLn "Applicative:"
     putStrLn . show $ parse (pure '1') "abc"
+    putStrLn . show $ parse (lengthParser <*> crappyParser) "abcdef"
+    putStrLn . show $ parse three "abcdef"
+
+    putStrLn "Monad:"
+    putStrLn . show $ parse (return '1') "abc"
+    putStrLn . show $ parse (item >>= (\x -> crappyParser)) "abcdef"
+    putStrLn . show $ parse (item >>= uselessParser) "abcdef"
